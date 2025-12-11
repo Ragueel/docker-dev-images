b861ee842147b0d57b160914b7a9b5b6c103d423
Raguel <kayne787@gmail.com>
2025-12-11 14:48:56 +0500

    update php version

--- php/php8.2/setup.sh
+++ php/php8.2/setup.sh
@@ -12,9 +12,9 @@ export DEBIAN_FRONTEND=noninteractive
 # install php dependencies
 echo "Installing php"
 add-apt-repository ppa:ondrej/php
-apt-get install -y php8.2 php8.2-dev
+apt-get install -y php8.4 php8.4-dev
 echo "Installing php dependencies"
-apt-get install -y php8.2-bcmath php8.2-zip php8.2-xml php8.2-mysql php8.2-sqlite php8.2-curl php8.2-cli php8.2-mbstring php8.2-gd
+apt-get install -y php8.4-bcmath php8.4-zip php8.4-xml php8.4-mysql php8.4-sqlite php8.4-curl php8.4-cli php8.4-mbstring php8.4-gd
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/bin --filename=composer
 echo "Installed php"
 
